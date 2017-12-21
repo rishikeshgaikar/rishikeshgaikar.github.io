894574eab3f9e6c55d8a031192db049d91f37550
rishikesh <rishikeshgaikar07@gmail.com>
2017-12-22 01:06:12 +0530

    contact form edited & comtact form php chnaged

--- contact_me.php
+++ contact_me.php
@@ -1,21 +1,47 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-	echo "No arguments Provided!";
-	return false;
+$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+
+    // Check whether submitted data is not empty
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClass = 'errordiv';
+        }else{
+            // Recipient email
+            $toEmail = 'user@example.com';
+            $emailSubject = 'Contact Request Submitted by '.$name;
+            $htmlContent = '<h2>Contact Request Submitted</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Subject</h4><p>'.$subject.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+
+            // Set content-type header for sending HTML email
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+            // Additional headers
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Your contact request has been submitted successfully !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Your contact request submission failed, please try again.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Please fill all the fields.';
+        $msgClass = 'errordiv';
+    }
 }
-
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-
-// Create the email and send the message
-$to = 'rishikeshgaikr07@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form: $name";
-$email_body = "You have received a new message from your Github website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@rishikeshgaikr.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to, $email_subject, $email_body, $headers);
-return true;
-
 ?>
